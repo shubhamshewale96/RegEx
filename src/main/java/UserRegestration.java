b51dacdc5d70fd1ec928bd32d726972e53ea8e88
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-18 16:26:05 +0530

    add:check mobile number validation

--- src/main/java/UserRegestration.java
+++ src/main/java/UserRegestration.java
@@ -40,14 +40,30 @@ public class UserRegestration {
 	/*
 	 * Email Validation
 	 */
-	private void email() {
+	private void emailValidation() {
 		String email;
 		Scanner get = new Scanner(System.in);
-		System.out.println("Enter LastName: \t note:First Latter should capital");
+		System.out.println("Enter Email Id: \t Eg:abc.xyz@bl.co.in");
 		email = get.next();
 		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}?(.[a-z]{2,3}){0,1}$";
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(email);
+		if (matcher.find())
+			System.out.println("Pattern matched");
+		else
+			System.out.println("Pattern not matched");
+	}
+	/*
+	 * Mobile number Validation
+	 */
+	private void mobileValidation() {
+		String mNumber;
+		Scanner get = new Scanner(System.in);
+		System.out.println("Enter Mobile Number : \t Eg:91 8542365232");
+		mNumber = get.next();
+		String regex = "^[0-9]{2}\\s{1}[0-9]{10}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(mNumber);
 		if (matcher.find())
 			System.out.println("Pattern matched");
 		else
@@ -59,6 +75,7 @@ public class UserRegestration {
 		UserRegestration userRegestration = new UserRegestration();
 		userRegestration.firstName();
 		userRegestration.lastName();
-		userRegestration.email();
+		userRegestration.emailValidation();
+		userRegestration.mobileValidation();
 	}
 }
