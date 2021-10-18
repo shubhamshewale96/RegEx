9aae4c8385a5de1706235e7d8bebd0ea14591465
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-18 16:16:02 +0530

    add:check email validation

--- src/main/java/UserRegestration.java
+++ src/main/java/UserRegestration.java
@@ -3,36 +3,62 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegestration {
+	/*
+	 * first name Validation
+	 */
 	private void firstName() {
 		String firstName;
 		Scanner get = new Scanner(System.in);
 		System.out.println("Enter FirstName: \t note:First Latter should capital");
 		firstName = get.next();
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(firstName);
-        if (matcher.find())
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(firstName);
+		if (matcher.find())
 			System.out.println("Pattern matched");
 		else
 			System.out.println("Pattern not matched");
-    }
+	}
+
+	/*
+	 * last name Validation
+	 */
 	private void lastName() {
 		String lastName;
 		Scanner get = new Scanner(System.in);
 		System.out.println("Enter LastName: \t note:First Latter should capital");
 		lastName = get.next();
-        String regex = "^[A-Z]{1}[a-z]{2,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(lastName);
-        if (matcher.find())
+		String regex = "^[A-Z]{1}[a-z]{2,}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(lastName);
+		if (matcher.find())
 			System.out.println("Pattern matched");
 		else
 			System.out.println("Pattern not matched");
-    }
+	}
+
+	/*
+	 * Email Validation
+	 */
+	private void email() {
+		String email;
+		Scanner get = new Scanner(System.in);
+		System.out.println("Enter LastName: \t note:First Latter should capital");
+		email = get.next();
+		String regex = "^[a-z0-9]{3,}+([_+-.][a-z0-9]{3,}+)*@[a-z0-9]+.[a-z]{2,3}?(.[a-z]{2,3}){0,1}$";
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(email);
+		if (matcher.find())
+			System.out.println("Pattern matched");
+		else
+			System.out.println("Pattern not matched");
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to regex");
 		UserRegestration userRegestration = new UserRegestration();
 		userRegestration.firstName();
 		userRegestration.lastName();
+		userRegestration.email();
 	}
 }
