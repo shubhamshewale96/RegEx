08052e852dcbb30f515dae9fda35070dc4506d19
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-10-18 16:04:35 +0530

    add:check Last name

--- src/main/java/UserRegestration.java
+++ src/main/java/UserRegestration.java
@@ -3,10 +3,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegestration {
-	String firstName;
-	public void firstName() {
+	private void firstName() {
+		String firstName;
 		Scanner get = new Scanner(System.in);
-		System.out.println("Enter Name: \t note:First Latter should capital");
+		System.out.println("Enter FirstName: \t note:First Latter should capital");
 		firstName = get.next();
         String regex = "^[A-Z]{1}[a-z]{2,}$";
         Pattern pattern = Pattern.compile(regex);
@@ -16,9 +16,23 @@ public class UserRegestration {
 		else
 			System.out.println("Pattern not matched");
     }
+	private void lastName() {
+		String lastName;
+		Scanner get = new Scanner(System.in);
+		System.out.println("Enter LastName: \t note:First Latter should capital");
+		lastName = get.next();
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(lastName);
+        if (matcher.find())
+			System.out.println("Pattern matched");
+		else
+			System.out.println("Pattern not matched");
+    }
 	public static void main(String[] args) {
 		System.out.println("Welcome to regex");
 		UserRegestration userRegestration = new UserRegestration();
 		userRegestration.firstName();
+		userRegestration.lastName();
 	}
 }
